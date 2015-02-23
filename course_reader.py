fdcce10e400d36b879b25355772a967659b5d624
JoeWeate <jfw6fp@virginia.edu>
2015-02-23 15:46:28 -0500

    commit all

--- course_reader.py
+++ course_reader.py
@@ -1,3 +1,7 @@
 #joey weate
+import csv, sqlite3
 
 def load_course_database(db_name, csv_filename):
+    with open('csv_filename', 'rU') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
